6fe63e5ed2a79d0ed73dbb11057f7aebcd3fd39e
RicardoMGM <ricardo.manel@gmail.com>
2020-08-26 16:56:11 -0300

    Conexão com a api

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
@@ -8,9 +8,24 @@ import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import api from '../../services/api';
 
 function Landing() {
     const {navigate} = useNavigation();
+
+    const [ totalConnections, setTotalConnections] = useState(0);
+
+    // UseEffect recebe dois parâmetros:
+    // 1º A função que será executada
+    // 2º Quando será executada a função, é uam array de dependências,
+    // que verifica quando os valores foram alterados. Deixar vazio executa apenas uma vez. 
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const {total} = response.data;
+            console.log(response);
+            setTotalConnections(total);
+        });
+    }, []);
 
     function hadnleNvigateToGiveClassesPage(){
         navigate('GiveClasses');
@@ -43,7 +58,7 @@ function Landing() {
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 285 conexões já realizadas {' '}
+                Total de {totalConnections} conexões já realizadas {' '}
                 <Image source={heartIcon} />
             </Text>
         </View>
