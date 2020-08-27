60b126ec14646d7bd7df3ec7578b047d81473445
RicardoMGM <ricardo.manel@gmail.com>
2020-08-27 17:55:53 -0300

    Adição da listagem de professores e botão whatsapp

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Linking } from 'react-native';
 
 import styles from './styles';
 import { RectButton } from 'react-native-gesture-handler';
@@ -8,31 +8,44 @@ import heartOutLineIcon from '../../assets/images/icons/heart-outline.png';
 import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsAppIcon from '../../assets/images/icons/whatsapp.png';
 
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
 
-function TeacherItem(){
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) =>{
+    function handleToWhatsApp(){
+        Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
                 <Image style={styles.avatar} 
-                    source={{uri: 'https://github.com/diego3g.png'}}
+                    source={{uri: teacher.avatar}}
                 />
 
                 <View style={styles.profileInfo}>
-                    <Text style={styles.name}>Diego Fernandes</Text>
-                    <Text style={styles.subject}>Química</Text>
+                    <Text style={styles.name}>{teacher.name}</Text>
+                    <Text style={styles.subject}>{teacher.subject}</Text>
                 </View>
             </View>
 
-            <Text style={styles.bio}>
-                Teste de bio, não copiei a bio de exempolo sem graça.
-                {'\n'} {'\n'}
-                1,2,3, testando...
-            </Text>
+            <Text style={styles.bio}>{teacher.bio}</Text>
 
             <View style={styles.footer}>
                 <Text style={styles.price}>
                     Preço/hora {'  '}
-                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                    <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
                 </Text>
                 
                 <View style={styles.buttonsContainer}>
@@ -41,7 +54,8 @@ function TeacherItem(){
                         <Image source={unFavoriteIcon} />
                     </RectButton>
 
-                    <RectButton style={styles.contactButton}>
+                    <RectButton onPress={handleToWhatsApp}
+                        style={styles.contactButton}>
                         <Image source={whatsAppIcon} />
                         <Text style={styles.contactButtonText}>Entrar em contato</Text>
                     </RectButton>
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,16 +2,34 @@ import React, { useState } from 'react';
 import { View, ScrollView, Text } from 'react-native';
 import styles from './styles';
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import { TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import api from '../../services/api';
 
 
 function TeacherList() {
+    const [teathers, setTeachers] = useState([]);
     const [isFiltersVisible, setIsFilterVisible] = useState(false);
+
+    const [ subject, setSubject ] = useState('');
+    const [ week_day, setWeekDay ] = useState('');
+    const [ time, setTime ] = useState('');    
 
     function handleToggleFiltersVisible() {
         setIsFilterVisible(!isFiltersVisible);
+    }
+
+    async function handleFilterSubmit() {
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time,
+            }
+        })
+        setIsFilterVisible(false);
+        setTeachers(response.data);
     }
 
     return (
@@ -27,6 +45,8 @@ function TeacherList() {
                         <Text style={styles.label}>Matéria</Text>
                         <TextInput 
                             style={styles.input}
+                            value={subject}
+                            onChangeText={text => setSubject(text)}
                             placeholder="Qual a matéria?"
                             placeholderTextColor="#c1bccc"
                         />
@@ -36,6 +56,8 @@ function TeacherList() {
                                 <Text style={styles.label}>Dia da semana</Text>
                                 <TextInput
                                     style={styles.input}
+                                    value={week_day}
+                                    onChangeText={week_day => setWeekDay(week_day)}
                                     placeholder="Qual o dia?"
                                     placeholderTextColor="#c1bccc"
 
@@ -46,6 +68,8 @@ function TeacherList() {
                                 <Text style={styles.label}>Horário</Text>
                                 <TextInput
                                     style={styles.input}
+                                    value={time}
+                                    onChangeText={time => setTime(time)}
                                     placeholder="Qual horário?"
                                     placeholderTextColor="#c1bccc"
 
@@ -53,7 +77,8 @@ function TeacherList() {
                             </View>
                         </View>
 
-                        <RectButton style={styles.submitButton}>
+                        <RectButton onPress={handleFilterSubmit}
+                            style={styles.submitButton}>
                             <Text style={styles.submitButtonText}>
                                 Filtrar
                             </Text>
@@ -67,13 +92,9 @@ function TeacherList() {
                     paddingHorizontal: 16,
                     paddingBottom: 16,
                 }}>
-
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                    {teathers.map((teacher: Teacher) => {
+                        return <TeacherItem key={teacher.id} teacher={teacher}/>
+                    })}
             </ScrollView>
           
         </View>
